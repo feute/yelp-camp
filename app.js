71e32a6b91012a92b114dbccc1d650fe89e67227
Angel Gomez <jangelgr7@gmail.com>
2018-10-13 13:07:54 -0400

    chore: initial routes (campgrounds)

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('landing page');
+  res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+  var campgrounds = [
+    { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2408b72d00a5efaf0f2e14d02f144790&auto=format&fit=crop&w=500&q=60' },
+    { name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=343c64df1b43f50769656d03c2b9f523&auto=format&fit=crop&w=500&q=60' },
+    { name: 'Mountain Goat\'s Rest', image: 'https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5cedc6b95f731395da7269d2341f9a5e&auto=format&fit=crop&w=500&q=60' }
+  ];
+
+  res.render('campgrounds', { campgrounds: campgrounds });
 });
 
 app.listen(port, function() {
